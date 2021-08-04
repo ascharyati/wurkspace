92ae615c57c34dec0025d865165222d81ed196e5
ascharyati <ascharya.prasad@technoidentity.com>
2021-08-05 01:51:46 +0530

    aa

---
+++ app/auth/components/dashboard/InsightsCard.tsx
@@ -0,0 +1,15 @@
+export const GoalsCard = () => {
+  return (
+    <>
+      <div className="m-2">
+        <div className="bg-white p-1 rounded shadow-lg relative hover:shadow-2xl">
+          <p className="text-black mb-1 text-xs text-left">
+            Create basic card for youl.Create basic card for youl
+          </p>
+          <div className="mx-1 pt-1 "></div>
+        </div>
+      </div>
+    </>
+  )
+}
+export default GoalsCard
